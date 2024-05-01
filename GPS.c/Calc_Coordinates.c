789b7ed07f21b14480fac6b0f9b6d2f49f199d92
MoAmrAwny <159016297+MoAmrAwny@users.noreply.github.com>
2024-05-02 02:33:10 +0300

    Update Calc_Coordinates.c

--- GPS.c/Calc_Coordinates.c
+++ GPS.c/Calc_Coordinates.c
@@ -9,7 +9,7 @@ void Calc_Coord (char GPS_arr[],float coordinates[])
     const int len = 80;
     char GPS_Elements[15][15];
     // Tokenization
-    char *token = strtok(GPS_arr, ",");
+    char *token = strtok(GPS_arr, ","); 
     int i = 0;
     while (token != NULL && i < 15) 
     {
@@ -19,24 +19,24 @@ void Calc_Coord (char GPS_arr[],float coordinates[])
     }
 
     // Data extraction
-    if (strcmp(GPS_Elements[1], "A") == 0) 
+    if (strcmp(GPS_Elements[1], "A") == 0) // strcmp() vif equal returns 0
       {
-            if (strcmp(GPS_Elements[3], "N") == 0) {
+            if (strcmp(GPS_Elements[3], "N") == 0) { // incase of North the value will be positive
     					  coordinates[0]= atof(GPS_Elements[2]);
     						UART_OutString(GPS_Elements[2]);
             }
     				else {
-                coordinates[0]= -atof(GPS_Elements[2]);
+                coordinates[0]= -atof(GPS_Elements[2]); // incase of South the value will be negative
     					
             }
     
-            if (strcmp(GPS_Elements[5], "E") == 0) {
+            if (strcmp(GPS_Elements[5], "E") == 0) { // incase of East the value will be positive
     				
               coordinates[1]= atof(GPS_Elements[4]);
     				 	
             } 
     				else{
-              coordinates[1]= -atof(GPS_Elements[4]);
+              coordinates[1]= -atof(GPS_Elements[4]); // incase of West the value will be negative
     					
             }
    
