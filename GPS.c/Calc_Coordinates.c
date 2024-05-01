d3111b404ee5834da91c3d3ebd48c63b3a09eccb
MoAmrAwny <159016297+MoAmrAwny@users.noreply.github.com>
2024-05-02 00:23:05 +0300

    Update and rename Calc_Coordinates to Calc_Coordinates.c

---
+++ GPS.c/Calc_Coordinates.c
@@ -0,0 +1,45 @@
+
+void Calc_Coord (char GPS_arr[],float coordinates[])
+{
+// *  logname  date      lattitude      longitude
+// *  $GPRMC,043438.00,A,1833.62410,N,07354.76336,E,0.024,,070319,,,A*78
+//                     |            |             |
+//                    valid        North         East
+
+    const int len = 80;
+    char GPS_Elements[15][15];
+    // Tokenization
+    char *token = strtok(GPS_arr, ",");
+    int i = 0;
+    while (token != NULL && i < 15) 
+    {
+        strcpy(GPS_Elements[i], token);
+        token = strtok(NULL, ",");
+        i++;
+    }
+
+    // Data extraction
+    if (strcmp(GPS_Elements[1], "A") == 0) 
+      {
+            if (strcmp(GPS_Elements[3], "N") == 0) {
+    					  coordinates[0]= atof(GPS_Elements[2]);
+    						UART_OutString(GPS_Elements[2]);
+            }
+    				else {
+                coordinates[0]= -atof(GPS_Elements[2]);
+    					
+            }
+    
+            if (strcmp(GPS_Elements[5], "E") == 0) {
+    				
+              coordinates[1]= atof(GPS_Elements[4]);
+    				 	
+            } 
+    				else{
+              coordinates[1]= -atof(GPS_Elements[4]);
+    					
+            }
+   
+     }
+    
+}
