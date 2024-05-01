cb4e36b71fdd9f1f47289424448e2d546de0c641
gannagawish <153089309+gannagawish@users.noreply.github.com>
2024-05-02 02:47:04 +0300

    Update GPS_Output.c

--- GPS.c/GPS_Output.c
+++ GPS.c/GPS_Output.c
@@ -1,49 +1,44 @@
-
 float* GPS_OUTPUT() {
-				
-        static float coordinates[2]; // Array to hold latitude and longitude values
-			
-        char GPS_arr[80]; // Local array to store GPS data
+	
+        static float coordinates[2];    // Array to hold latitude and longitude values
+        char GPS_arr[80];              // Local array to store received GPS data
         char received_char;
-        char i;
-        int log=1;
-			  char GPS_logname[]="$GPRMC,";
-        // Loop until we receive the required logname
-        do {
-						log=1;
-						 UART_OutString("YOU ENTERED DO WHILE\n");
-					   	getCommand(command, len);
-						  UART_OutString("\n");
+        char i;                        //used for iteration over for_loop below
+        int flag=1;
+	char GPS_logname[]="$GPRMC,"; //the required logname we want to receive
+
+do {                         // Loop until we receive the required logname
+	     flag=1;
+	     UART_OutString("YOU ENTERED THE FIRST DO WHILE\n");
+	     getCommand(command, len);
+	     UART_OutString("\n");
             for (i = 0; i < 7; i++) {
-							UART_OutString("YOU ENTERED FOR LOOP\n");
-                if (command[i]!=GPS_logname[i]) {//WHEN WE WANT TO TEST GPS WE WILL CHANGE COMMAND TO UART READ
-									UART_OutString("YOU ENTERED IF\n");
+		   UART_OutString("YOU ENTERED FOR LOOP\n");
+                if (command[i]!=GPS_logname[i]) {           //WHEN WE WANT TO TEST GPS WE WILL CHANGE COMMAND TO UART READ "manual testing"
+		   UART_OutString("YOU ENTERED IF\n");
                    log=0;
-									break;
+                   break;
                 }
 							
-           }
+             }   
 					
-        } while(log==0);
+   } while(log==0);
 				
-					UART_OutString("YOU EXITED DO WHILE\n");
-        // Initialize GPS array
-        strcpy(GPS_arr, "");
-
-        // Read GPS data until '*' is received
-        while ( received_char != '*')
-        {
-						UART_OutString("YOU ENTERED THE SEC DO WHILE\n");
-            received_char = command[i];
-            strcat(GPS_arr, &received_char); // Append received character to GPS array
-					i++;
+	UART_OutString("YOU EXITED THE FIRST DO WHILE\n");            
+        strcpy(GPS_arr, "");                               // Initialize GPS array
+        while ( received_char != '*'){                     // Read GPS data until '*' is received
+	    UART_OutString("YOU ENTERED THE SECOND WHILE LOOP\n");
+            received_char = command[i];  
+            strcat(GPS_arr, &received_char);              // Append received character to GPS array
+	     i++;
         } 
 				
-				UART_OutString("YOU EXITED THE SEC DO WHILE\n");
-				UART_OutString(GPS_arr);
-        // Process GPS data and extract latitude and longitude
-        Calc_Coord(GPS_arr,coordinates);
+	  UART_OutString("YOU EXITED THE SECOND DO WHILE\n");
+	  UART_OutString(GPS_arr);
+        
+        Calc_Coord(GPS_arr,coordinates);                  // Process GPS data and extract latitude and longitude
 
         return coordinates;
 }
+      
 		
