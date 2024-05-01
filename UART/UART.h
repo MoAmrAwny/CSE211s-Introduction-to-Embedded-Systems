566384be0a7e976355fe2536b8ddd3ab0e190a6f
MoAmrAwny <159016297+MoAmrAwny@users.noreply.github.com>
2024-05-02 02:16:16 +0300

    Update UART.h

--- UART/UART.h
+++ UART/UART.h
@@ -1,1 +1,14 @@
+#include "tm4c123gh6pm.h"
+#define CR 0x0D	
+
+//void UART_Init_PortD(void);
+void UART_Init_PortA(void);
+
+void UART_OutString(char *pt);
+void RGB_clearOutput(unsigned char data);
+void RGB_setOutput(unsigned char data);
+void getCommand(char *command, int length);
+void UART_OutChar(unsigned char data);
+char UART_InChar();
+
 
