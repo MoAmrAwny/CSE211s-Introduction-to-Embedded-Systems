ad88278c46be4986254226771cbe5d1716d93586
gannagawish <153089309+gannagawish@users.noreply.github.com>
2024-05-02 02:51:01 +0300

    GPIO.c

---
+++ GPIO/GPIO.c
@@ -0,0 +1,37 @@
+#include "GPIO.h"
+
+
+void portF_Init(void) {
+
+	SYSCTL_RCGCGPIO_R |= 0X20;  //port f clk enable
+	while((SYSCTL_PRGPIO_R& 0X20)==0); //delay
+	GPIO_PORTF_LOCK_R = GPIO_LOCK_KEY;
+	GPIO_PORTF_CR_R |= 0X0E;
+	GPIO_PORTF_AMSEL_R &=~ 0X0E;
+	GPIO_PORTF_PCTL_R &=~ 0X0000FFF0;
+	GPIO_PORTF_AFSEL_R &=~ 0X0E;
+	GPIO_PORTF_DIR_R |= 0X0E;
+	GPIO_PORTF_DEN_R |= 0X0E;
+	GPIO_PORTF_DATA_R &=~0X0E;
+ 
+}
+
+void SW2_Init(void){
+
+	GPIO_PORTF_LOCK_R = GPIO_LOCK_KEY ; // unlock portF
+	GPIO_PORTF_CR_R |= PF_SW2_mask ; // allow changes on SW2 PF0
+	GPIO_PORTF_AMSEL_R &= ~(PF_SW2_mask); // 0x01 = PF_SW2_mask , disable the analog input
+	GPIO_PORTF_AFSEL_R &= ~(PF_SW2_mask); // 0x01 = PF_SW2_mask , disable the alternate functions
+	GPIO_PORTF_PCTL_R &= ~(0x0000000F); // clear the desired bits PCTL PF0 
+	GPIO_PORTF_DIR_R &= ~PF_SW2_mask ; // enable direction of I/O , set the SW2 pin to be input
+	GPIO_PORTF_DEN_R |= PF_SW2_mask ; // enable digital I/O
+	GPIO_PORTF_PUR_R |= PF_SW2_mask ; // pull up the switch 
+
+}
+
+unsigned char read_switch2(){
+
+	return GPIO_PORTF_DATA_R &0x01;
+
+}
+
