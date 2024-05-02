b41cf6e2b68da1c9d74d062d3d5a09c91361f46e
gannagawish <153089309+gannagawish@users.noreply.github.com>
2024-05-02 03:04:16 +0300

    GPS.h

--- GPS.c/GPS.h
+++ GPS.c/GPS.h
@@ -1,1 +1,20 @@
+#include "tm4c123gh6pm.h"
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 
+#define logname_length        7
+#define pi                    3.14159265359
+#define Earth_Radius          6371000
+#define finalDestinationLat   30.0640669
+#define finalDestinationLong  31.2804439
+
+
+
+float toRad(float angle);
+float getDistance(float currentLong, float currentLat, float desiredLong, float desiredLat);
+char UART_Read ();
+float* GPS_OUTPUT();
+void Calc_Coord ();
+float totalDistance(float totalDisplacement);
