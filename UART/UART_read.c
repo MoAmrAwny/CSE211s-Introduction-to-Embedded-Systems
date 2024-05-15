9e60b48bcd9f8fa2feb3c390e841e43ffe4db046
OmarMansourAhmed <168648445+OmarMansourAhmed@users.noreply.github.com>
2024-05-15 13:56:09 +0300

    Delete UART/UART_read.c

--- UART/UART_read.c
+++
@@ -1,8 +0,0 @@
-
-char UART_Read () 
-{
-	
-  while((UART0_FR_R &0x10)!=0);     // check if the buffer is empty
-  return (char)(UART0_DR_R & 0xFF) ;//return the first 8 bits that contains Data
-
-}
