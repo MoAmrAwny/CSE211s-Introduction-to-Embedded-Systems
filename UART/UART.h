bf149ad48db0e32cc19b6837e3802e6f3a13385d
gannagawish <153089309+gannagawish@users.noreply.github.com>
2024-05-16 20:46:21 +0300

    Update UART.h

--- UART/UART.h
+++ UART/UART.h
@@ -1,14 +1,28 @@
-#include "tm4c123gh6pm.h"
-#define CR 0x0D	
-
-//void UART_Init_PortD(void);
-void UART_Init_PortA(void);
-
-void UART_OutString(char *pt);
-void RGB_clearOutput(unsigned char data);
-void RGB_setOutput(unsigned char data);
-void getCommand(char *command, int length);
-void UART_OutChar(unsigned char data);
-char UART_InChar();
+#ifndef UART
+#define UART
 
 
+///*	UART 0 Init	*///
+void UART0_Init(void);
+
+///*	UART 0 functions	*///
+char UART0_getChar();
+void UART0_OutChar(char data);
+void UART0_OutString(char *pt);
+void GetCommand_UART0(char *Command,int len );
+
+void GetCommand_UART1_Modified(char *Command,int len );
+
+
+///*	UART 1 Init	*///
+void UART1_Init (void);
+
+///*	UART 1 functions	*///
+char UART1_getChar();
+void UART1_OutChar(char data);
+void UART1_OutString(char *pt);
+void GetCommand_UART1(char *Command,int len );
+
+
+
+#endif
