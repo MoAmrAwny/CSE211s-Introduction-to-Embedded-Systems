d834d741003fe1d1ead84173ce1398f55da5f2cd
MoAmrAwny <159016297+MoAmrAwny@users.noreply.github.com>
2024-05-16 21:08:09 +0300

    Update LCD.h

--- LCD/LCD.h
+++ LCD/LCD.h
@@ -1,1 +1,34 @@
+#ifndef LCD
+#define LCD
+
+
+
+///***************** Prototypes *****************///
+
+void PrintData(char data);
+void LCD_Data(char data);
+void LCD_Cmd(unsigned char cmd);
+void LCD_String(char *str , char len);
+void LCD_String_modified(char *str);
+void LCD_Init(void);
+void Pins_Init();	
+void floatToString(float num, char *str, int precision);	
+
+
+  // LCD Installation
+	
+	// RS = PD0
+	// RW = PD1
+	// EN = PD2
+	
+	// D0 = PA7
+	// D1 = PA6
+	// D2 = PA5
+	// D3 = PB4
+	// D4 = PE5
+	// D5 = PE4
+	// D6 = PE1
+	// D7 = PD3
+	
+#endif
 
