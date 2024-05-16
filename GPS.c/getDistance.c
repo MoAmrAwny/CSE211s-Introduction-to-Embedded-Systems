e6e689007fe19f93da5873b0d73f4624a1dc264c
OmarMansourAhmed <168648445+OmarMansourAhmed@users.noreply.github.com>
2024-05-16 21:40:01 +0300

    Update getDistance.c

--- GPS.c/getDistance.c
+++ GPS.c/getDistance.c
@@ -1,17 +1,11 @@
-//////////////////////////convert angle to radian////////////////////////////
-float toRad(float angle){
-    return angle * (pi/180);
-}
-
-/*The haversine formula determines the great-circle distance between two points on a sphere given their longitudes and latitudes.*/
 
 float getDistance(float currentLat, float currentLong,float desiredLat, float desiredLong)
 {
     // get radian angle
-    float currentLongRad = toRad(currentLong);   /*  convert coordinates to radian   */
-    float currentLatRad = toRad(currentLat);
-    float desiredLongRad =toRad(desiredLong);
-    float desiredLatRad = toRad(desiredLat);
+    float currentLongRad = toRad(ToDegree(currentLong));   /*  convert coordinates to radian   */
+    float currentLatRad = toRad(ToDegree(currentLat));
+    float desiredLongRad =toRad(ToDegree(desiredLong));
+    float desiredLatRad = toRad(ToDegree(desiredLat));
 
     // get difference
     float longDifference = desiredLongRad - currentLongRad;
