e64eba284e0ff7bddf99cc05ebfd1cb884b62636
OmarMansourAhmed <168648445+OmarMansourAhmed@users.noreply.github.com>
2024-05-16 21:40:43 +0300

    Update GPS.h

--- GPS.c/GPS.h
+++ GPS.c/GPS.h
@@ -1,20 +1,32 @@
+#ifndef GPS
+#define GPS
+
+#include <stdbool.h>
+
+#define PI 3.141592653589793238462643383279502884197
+#define Earth_Radius  	6371000
+
 #include "tm4c123gh6pm.h"
 #include <math.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#define logname_length 7
+#define pi   3.14159265359
+#define Earth_Radius    6371000
 
-#define logname_length        7
-#define pi                    3.14159265359
-#define Earth_Radius          6371000
-#define finalDestinationLat   30.0640669
-#define finalDestinationLong  31.2804439
+#define finalDestinationLat 30.2460663
+#define finalDestinationLong 31.4666137
 
 
-
+float ToDegree (float angle);
 float toRad(float angle);
-float getDistance(float currentLong, float currentLat, float desiredLong, float desiredLat);
-char UART_Read ();
+float getDistance(float currentLat, float currentLong,float desiredLat, float desiredLong);
+
 float* GPS_OUTPUT();
-void Calc_Coord ();
-float totalDistance(float totalDisplacement);
+
+void printFloat(float a);
+float getDisplacement(float currentLat, float currentLong,float desiredLat, float desiredLong);
+float str_to_float(const char *str);
+
+#endif	
